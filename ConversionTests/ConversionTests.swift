64faea59f3cc0dd8a0cec8260622f1bcc87e2e47
Aleksandr <govoruhin95@yandex.ru>
2020-04-16 12:48:50 +0700

    Initial Commit

---
+++ ConversionTests/ConversionTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConversionTests.swift
+//  ConversionTests
+//
+//  Created by Александр Говорухин on 16.04.2020.
+//  Copyright © 2020 Александр Говорухин. All rights reserved.
+//
+
+import XCTest
+@testable import Conversion
+
+class ConversionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
